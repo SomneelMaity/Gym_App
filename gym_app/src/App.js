5ac7aabd87722005036b7a54ad5acf31727e687b
SomneelMaity <somneel.maity@gmail.com>
2024-02-25 15:46:37 +0530

    Reasons Section

--- gym_app/src/App.js
+++ gym_app/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import Hero from './components/Hero/Hero.jsx';
-import Programs from './components/Programs/Programs.jsx';
+import "./App.css";
+import Hero from "./components/Hero/Hero.jsx";
+import Programs from "./components/Programs/Programs.jsx";
+import Reasons from "./components/Reasons/Reasons.jsx";
+
+
 function App() {
   return (
     <div className="App">
-          <Hero />
-          <Programs />
+      <Hero />
+      <Programs />
+      <Reasons />
     </div>
   );
 }
